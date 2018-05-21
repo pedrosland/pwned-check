937eaa097e6bb2b41470ba7b490b058d52d3ffd4
Pedro Sland <pedrosland@gmail.com>
2018-05-21 21:16:15 +0100

    Fix protoc

--- .travis/get-protoc.sh
+++ .travis/get-protoc.sh
@@ -5,7 +5,4 @@ VERSION="3.5.1"
 cd /tmp
 
 curl -LO https://github.com/google/protobuf/releases/download/v${VERSION}/protoc-${VERSION}-linux-x86_64.zip
-unzip protoc-${VERSION}-linux-x86_64.zip -d protoc3
-
-mv protoc3/bin/* /usr/local/bin/
-mv protoc3/include/* /usr/local/include/
+unzip protoc-${VERSION}-linux-x86_64.zip -d $HOME/protoc3
