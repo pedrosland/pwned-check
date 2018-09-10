c01cde37a55a46c9a75a63227bef24ebaace13d9
Pedro Sland <pedrosland@gmail.com>
2018-09-10 23:43:10 +0100

    Update to latest list of hashes

--- .shippable/get-pwned-passwords.sh
+++ .shippable/get-pwned-passwords.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ ! -f "$HOME/pwned-passwords/pwned-passwords-2.0.txt.7z" ]; then
+if [ ! -f "$HOME/pwned-passwords/pwned-passwords-ordered-by-count.7z" ]; then
     mkdir -p $HOME/pwned-passwords
-    curl -L -o $HOME/pwned-passwords/pwned-passwords-2.0.txt.7z https://downloads.pwnedpasswords.com/passwords/pwned-passwords-2.0.txt.7z
+    curl -L -o $HOME/pwned-passwords/pwned-passwords-ordered-by-count.7z https://downloads.pwnedpasswords.com/passwords/pwned-passwords-ordered-by-count.7z
 fi
--- .shippable/save-filter.sh
+++ .shippable/save-filter.sh
@@ -3,4 +3,4 @@
 set -x
 set -e
 
-7za x $HOME/pwned-passwords/pwned-passwords-2.0.txt.7z -so | ./pwned-save -import-file=- -num-passwords=502000000 -save-file=pwned-data.bin
+7za x $HOME/pwned-passwords/pwned-passwords-ordered-by-count.7z -so | ./pwned-save -import-file=- -num-passwords=518000000 -save-file=pwned-data.bin
