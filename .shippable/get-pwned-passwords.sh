b14f904107ad31e11c6959366a97eb2becbbee18
Pedro Sland <pedrosland@gmail.com>
2019-06-17 20:09:43 +0100

    Update to password list v4

--- .shippable/get-pwned-passwords.sh
+++ .shippable/get-pwned-passwords.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-if [ ! -f "$HOME/pwned-passwords/pwned-passwords-ordered-by-count.7z" ]; then
+if [ ! -f "$HOME/pwned-passwords/pwned-passwords-sha1-ordered-by-count-v4.7z" ]; then
+    # Clear and re-create to avoid storing old versions
+    rm -rf $HOME/pwned-passwords
     mkdir -p $HOME/pwned-passwords
-    curl -L -o $HOME/pwned-passwords/pwned-passwords-ordered-by-count.7z https://downloads.pwnedpasswords.com/passwords/pwned-passwords-ordered-by-count.7z
+    curl -L -o $HOME/pwned-passwords/pwned-passwords-sha1-ordered-by-count-v4.7z https://downloads.pwnedpasswords.com/passwords/pwned-passwords-sha1-ordered-by-count-v4.7z
 fi
