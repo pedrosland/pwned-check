f5b5b6c46c6f424024702fbcf6547eced8e74b97
Pedro Sland <pedrosland@gmail.com>
2019-06-18 11:58:43 +0100

    Update correct number of passwords

--- .shippable/save-filter.sh
+++ .shippable/save-filter.sh
@@ -3,4 +3,4 @@
 set -x
 set -e
 
-7za x $HOME/pwned-passwords/$PWNED_FILENAME -so | ./pwned-save -import-file=- -num-passwords=518000000 -save-file=pwned-data.bin
+7za x $HOME/pwned-passwords/$PWNED_FILENAME -so | ./pwned-save -import-file=- -num-passwords=$NUM_PASSWORDS -save-file=pwned-data.bin
