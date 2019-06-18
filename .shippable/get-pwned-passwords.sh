94bb7831f32491edf4cb37b1444be02e5bbbd319
Pedro Sland <pedrosland@gmail.com>
2019-06-18 11:50:08 +0100

    Use correct compressed password file

--- .shippable/get-pwned-passwords.sh
+++ .shippable/get-pwned-passwords.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-if [ ! -f "$HOME/pwned-passwords/pwned-passwords-sha1-ordered-by-count-v4.7z" ]; then
+if [ ! -f "$HOME/pwned-passwords/$PWNED_FILENAME" ]; then
     # Clear and re-create to avoid storing old versions
     rm -rf $HOME/pwned-passwords
     mkdir -p $HOME/pwned-passwords
-    curl -L -o $HOME/pwned-passwords/pwned-passwords-sha1-ordered-by-count-v4.7z https://downloads.pwnedpasswords.com/passwords/pwned-passwords-sha1-ordered-by-count-v4.7z
+    curl -L -o $HOME/pwned-passwords/$PWNED_FILENAME https://downloads.pwnedpasswords.com/passwords/$PWNED_FILENAME
 fi
